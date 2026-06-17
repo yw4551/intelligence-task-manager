96f9dce153f8a5284767efc40eff27e1244688aa
Yitzi Weiner <yitziw123@gmail.com>
2026-06-17 15:26:28 +0300

    Final db_connection.py file

--- database/db_connection.py
+++ database/db_connection.py
@@ -20,8 +20,11 @@ class DB_connection:
             port = self.port
         )
 
+    def start_cursor(self):
+        return self.conn.cursor()
+
     def create_database(self):
-        cursor = self.conn.cursor()
+        cursor = self.start_cursor()
         cursor.execute(f"CREATE DATABASE IF NOT EXISTS {self.database}")
         cursor.execute(f"USE {self.database}")
         cursor.close()
@@ -36,7 +39,7 @@ class DB_connection:
                 is_active BOOLEAN DEFAULT TRUE,
                 completed_missions INT DEFAULT 0,
                 failed_missions INT DEFAULT 0,
-                agent_rank ENUM(Junior, Senior, Commander)
+                agent_rank ENUM("Junior", "Senior", "Commander")
             )
         """)
         cursor.execute("""
@@ -45,18 +48,18 @@ class DB_connection:
                 title VARCHAR(50) NOT NULL,
                 description TEXT NOT NULL,
                 location VARCHAR(100) NOT NULL,
-                difficulty INT NOT NULL,
-                importance INT NOT NULL,
-                status ENUM(NEW, IN_PROGRESS, COMPLETED, FAILED, CANCELLED) DEFAULT NEW
-                risk_level ENUM(LOW, MEDIUM, HIGH, CRITICAL)
-                assigned_agent_id INT DEFAULT NULL,
+                difficulty INT NOT NULL CHECK(difficulty >= 1) CHECK(difficulty <= 10),
+                importance INT NOT NULL CHECK(importance >= 1) CHECK(importance <= 10),
+                status ENUM("NEW", "IN_PROGRESS", "COMPLETED", "FAILED", "CANCELLED") DEFAULT "NEW",
+                risk_level ENUM("LOW", "MEDIUM", "HIGH", "CRITICAL"),
+                assigned_agent_id INT DEFAULT NULL
             )
         """)
         self.conn.commit()
         cursor.close()
         self.conn.close()
 
-    def connect_to_db(self, sql: str, values: tuple = None):
+    def connect_to_db(self, sql: str, values: tuple = None) -> bool:
         cursor = self.conn.cursor()
         cursor.execute(sql, values)
         self.conn.commit()
@@ -66,5 +69,25 @@ class DB_connection:
         self.conn.close()
         return last_id, count_rows
 
+    def fetch_all(self, sql: str, values: tuple = None) -> list[dict]:
+        cursor = self.start_cursor()
+        cursor.execute(sql, values)
+        data = cursor.fetchall()
 
-connection = DB_connection("127.0.0.1", "intelligence-mysql", 1234, "Intelligence_db", 3306)
+        if not data:
+            return []
+
+        return data
+
+    def fetch_one(self, sql: str, values: tuple = None) -> list[dict]:
+        cursor = self.start_cursor()
+        cursor.execute(sql, values)
+        data = cursor.fetchone()
+
+        if not data:
+            return []
+
+        return data
+
+
+connection = DB_connection("127.0.0.1", "root", "1234", "Intelligence_db", 3306)
